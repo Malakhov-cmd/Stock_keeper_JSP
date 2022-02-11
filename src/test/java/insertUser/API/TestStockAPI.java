29cb6dfcb5ef3fc8d4492bb74be3119891e3d10c
Malakhov-cmd <mgs-2000@yandex.ru>
2022-02-12 01:16:39 +0400

    refactor with Reactor Project

--- src/test/java/insertUser/API/TestStockAPI.java
+++ src/test/java/insertUser/API/TestStockAPI.java
@@ -1,7 +1,11 @@
 package insertUser.API;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stock.keeper.stockkeeper.service.DTO.StockApiDTO;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,19 +17,60 @@ import java.util.Date;
 
 public class TestStockAPI {
 
+    private final String KeyAPI_Origin = "S9xvRnDm5whNDqDXaGriI3Wbcohpqy84";
+    private final String KeyAPI_Support = "mayjkucLrChPDlhaTpnIqAC2hijvY1m0";
+    private final String KeyAPI_Error_Case = "xbC5mDo7lJT4o0W40xReBhBXp4rYgC3h";
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final Long twoDaysValue = 86_400_000L;
+
     @Test
-    public void getResponceByAPI(){
+    public void getResponceByAPI() {
         SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date();
 
-        try {
-            System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime()- 31_536_000_000L)));
+        Flux<String> ints = Flux.range(0, 4)
+                .map(i -> {
+                    Long pastTimeValue = 0L;
+                    String stockData = "";
+
+                    if (i == 0) pastTimeValue = 31_536_000_000L;
+                    else if (i == 3) pastTimeValue = 0L;
+                    else pastTimeValue = 31_536_000_000L - 7_884_000_000L * i;
+
+                    try {
+                        stockData = getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - pastTimeValue), KeyAPI_Origin);
+                    } catch (IOException e) {
+                        try {
+                            stockData = getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - pastTimeValue - twoDaysValue * 3), KeyAPI_Support);
+                        } catch (IOException ex) {
+                            try {
+                                stockData = getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - pastTimeValue + twoDaysValue * 3), KeyAPI_Error_Case);
+                            } catch (IOException exp) {
+                                System.out.println("Get");
+                            }
+                        }
+                    }
+                    return stockData;
+                });
+        ints.subscribe(stockDTO -> {
+            try {
+                StockApiDTO stockApiDTO = objectMapper.readValue(stockDTO, StockApiDTO.class);
+                System.out.println(stockApiDTO.toString());
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        });
+
+        /*try {
+            System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - 31_536_000_000L)));
         } catch (IOException e) {
             try {
-                System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime()- 31_536_000_000L)));
+                System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - 31_536_000_000L)));
             } catch (IOException ex) {
                 try {
-                    System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime()- 31_536_000_000L)));
+                    System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - 31_536_000_000L)));
                 } catch (IOException exc) {
                     System.err.println("Data is invalid");
                 }
@@ -64,35 +109,34 @@ public class TestStockAPI {
             System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - 86_400_000L)));
         } catch (IOException e) {
             try {
-                System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - 86_400_000L*2)));
+                System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - 86_400_000L * 2)));
             } catch (IOException ex) {
                 try {
-                    System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - 86_400_000L*3)));
+                    System.out.println(getStockDataByDateViaJSON("AAPL", formater.format(date.getTime() - 86_400_000L * 3)));
                 } catch (IOException exc) {
                     System.err.println("Data is invalid");
                 }
             }
-        }
+        }*/
     }
 
 
-    private String getStockDataByDateViaJSON(String index, String date)  throws IOException {
-        final URL url = new URL("https://api.polygon.io/v1/open-close/" + index +"/" + date + "?adjusted=false&apiKey=S9xvRnDm5whNDqDXaGriI3Wbcohpqy84");
+    private String getStockDataByDateViaJSON(String index, String date, String keyAPI) throws IOException {
+        final URL url = new URL("https://api.polygon.io/v1/open-close/" + index + "/" + date + "?adjusted=false&apiKey=" + keyAPI);
         final HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
         con.setRequestMethod("GET");
         con.setRequestProperty("Content-Type", "application/json");
 
-        try (final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-            String inputLine;
-            final StringBuilder content = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            return content.toString();
-        } catch (final Exception ex) {
-            ex.printStackTrace();
-            return "";
+        final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+        String inputLine;
+        final StringBuilder content = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
         }
+        return content.toString();
+
     }
 }
